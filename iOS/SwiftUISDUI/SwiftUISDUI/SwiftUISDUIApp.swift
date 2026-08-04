dcfb8467141edb23d8154420f2e44e7248f1c5f9
Darshan raju <rajdarshan1@gmail.com>
2026-08-04 20:29:40 +0530

    chore(ios): added iOS project

---
+++ iOS/SwiftUISDUI/SwiftUISDUI/SwiftUISDUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISDUIApp.swift
+//  SwiftUISDUI
+//
+//  Created by Darshan Raju on 04/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISDUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
